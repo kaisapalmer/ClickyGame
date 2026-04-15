f0d6321ed0e8f4a8d18af7dd07c0ad63a90890d6
kaisapalmer <kaisa.palmer@gmail.com>
2026-04-15 22:31:14 +0200

    Created button

--- js/app.js
+++ js/app.js
@@ -1,1 +1,8 @@
-console.log('Hello World!');
+// Click a button or object to earn points so that I can increase my score.
+// See my current score during the game so that I know how well I am doing.
+// See a countdown timer so that I know how much time is left.
+
+// Variables
+let score;
+
+// HTML DOM
